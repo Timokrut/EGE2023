f511cac21fda9ec8cde17e2d46404be97f7942e6
Timofey <potapov.tim14@gmail.com>
2023-06-07 19:26:17 +0300

    2

--- RNDM NUMB/trash/meow.py
+++ RNDM NUMB/trash/meow.py
@@ -1,19 +1,7 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    for j in range (2, int(n**0.5) + 1):
-        if n % j == 0:
-            return False
-    return True    
-
-
-k = 0
-
-for x in range(2422000, 2422081):
-    if is_prime(x) == 1:
-        k += 1
-        print(k, x)
-
-
-
-
+P = range(10, 41)
+Q = range(5, 16)
+R = range(35, 51)
+for x in [7, 12, 20, 37, 45]:
+    for ans in [True, False]:
+        if ((ans) or (x in P)) or ((x in Q) <= (x in R)):
+            print(x, ans)
