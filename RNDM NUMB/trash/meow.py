860bd544dbaa95802a716727dc79a328a35f663a
Timofey <potapov.tim14@gmail.com>
2023-06-12 12:08:38 +0300

    upd

--- RNDM NUMB/trash/meow.py
+++ RNDM NUMB/trash/meow.py
@@ -1,9 +1,43 @@
-def second_symbol(s, symbol):
+def count_div(n): #подсчет делителей
     k = 0
-    for i in range(len(s)):
-        if s[i] == symbol:
+    for  j in range(1, int(n**0.5) + 1):
+        if n % j == 0:
             k += 1
-            if k == 2:
-                return i
-    return -1
-print(second_symbol('qq', 'q'))            
+            if j != n // j:
+                k += 1
+    return k
+
+# print(count_div(100)) 
+# 9               
+
+
+def find_div(n): #найти делители
+    a = []
+    for j in range(1, int(n**0.5) + 1):
+        if n % j == 0:
+            a.append(j)
+            if j != n // j:
+                a.append(n//j)
+    a.sort()
+    return a   
+
+# print(find_div(100))
+# [1, 2, 4, 5, 10, 20, 25, 50, 100]
+
+
+def is_prime(n): #простое или нет
+    if n <= 1:
+        return False
+    for j in range(2, int(n**0.5) + 1):
+        if n % j == 0:
+            return False
+    return True        
+
+# print(is_prime(3))
+# True
+
+
+m = 0
+for x in range(84052, 84131):
+    if count_div(x) == 72:
+        print(x) 
